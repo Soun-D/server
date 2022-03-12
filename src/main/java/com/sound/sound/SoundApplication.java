194ebb6a2d968ff82a44f63191700cd0d93c400f
kwakdony <kwakdh25@gmail.com>
2022-03-12 23:46:45 +0900

    Initial Commit

---
+++ src/main/java/com/sound/sound/SoundApplication.java
@@ -0,0 +1,13 @@
+package com.sound.sound;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoundApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoundApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sound/sound/SoundApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sound.sound;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoundApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
